1710deea85039692cf28b4f1cfa829eddff5477d
ali varaste pour <alivaraste@outlook.com>
2022-07-09 04:06:37 -0700

    build mode is set to development

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: {
+      import: path.resolve(__dirname, "src/index.js"),
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+};
