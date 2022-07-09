1ee2c1181b676a1da0e107c4ba9223840e265eaf
ali varaste pour <alivaraste@outlook.com>
2022-07-09 03:22:11 -0700

    entry object is added to wepack configuration file

--- wepack.config.js
+++ wepack.config.js
@@ -1,3 +1,20 @@
 const path = require("path");
 
-module.exports = {};
+module.exports = {
+  /**
+   * defining entries can be done in several ways
+   */
+  // 1
+  //   entry: {
+  //     main: "./src/index.js",
+  //     second: "./path/to/another/entry",
+  //   },
+  // 2
+  // entry: "./src/index.js",
+  // 3 (better way)
+  entry: {
+    main: {
+      import: path.resolve(__dirname, "./src/index.js"),
+    },
+  },
+};
