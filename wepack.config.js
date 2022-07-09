09064c7f721f5fd9d5720b1af62d7f234ba99e68
ali varaste pour <alivaraste@outlook.com>
2022-07-09 03:47:19 -0700

    output object is added to wepack configuration file

--- wepack.config.js
+++ wepack.config.js
@@ -1,20 +1,14 @@
 const path = require("path");
 
 module.exports = {
-  /**
-   * defining entries can be done in several ways
-   */
-  // 1
-  //   entry: {
-  //     main: "./src/index.js",
-  //     second: "./path/to/another/entry",
-  //   },
-  // 2
-  // entry: "./src/index.js",
-  // 3 (better way)
   entry: {
     main: {
       import: path.resolve(__dirname, "./src/index.js"),
     },
+  },
+  output: {
+    path: path.resolve("__dirname", "./dist"), // the path to the output file(s)
+    filename: "bundle.js", // the name of the output file. for multiple output chunks it is better to use the below format
+    // filename: "[name].js", -> would become main.js in this code. now if we had another entry point named 'second', out output chucks would be sth like this: main.js - second.js
   },
 };
