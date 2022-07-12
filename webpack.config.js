7070e7dc4fc9c6eba614cf0677da10c23d367f42
ali varaste pour <alivaraste@outlook.com>
2022-07-12 00:23:23 -0700

    new loaders are added to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   },
   module: {
     rules: [{ test: /\.ts$/, use: ["ts-loader"] }],
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
   },
   plugins: [
     new HtmlWebpackPlugin({
