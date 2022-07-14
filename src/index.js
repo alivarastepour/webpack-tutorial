032e230de15220e7de32a9588b61dfc4b2550867
ali varaste pour <alivaraste@outlook.com>
2022-07-14 02:12:33 -0700

    content in src folder is changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./index.css";
 import photo from "./photo-1592853285454-34691b7b74c4.jpeg";
-
+import Data from "./index.csv";
 const container = document.getElementById("container");
 container.innerHTML = "سلام";
 container.style.color = "red";
@@ -10,3 +10,5 @@ const node = document.createElement("img");
 node.src = photo;
 node.classList.add("pic");
 container.appendChild(node);
+
+console.log(Data);
